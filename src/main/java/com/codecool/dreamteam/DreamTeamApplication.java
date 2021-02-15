9a7b5bd48f5ccb4ebd84f7006aededee24acfd69
Tharegon <dombovari11david@gmaa>
2021-02-15 16:21:50 +0100

    add CommandLineRunner to init a basic entity

--- src/main/java/com/codecool/dreamteam/DreamTeamApplication.java
+++ src/main/java/com/codecool/dreamteam/DreamTeamApplication.java
@@ -1,13 +1,36 @@
 package com.codecool.dreamteam;
 
+import com.codecool.dreamteam.entity.Card;
+import com.codecool.dreamteam.repository.CardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
 
 @SpringBootApplication
 public class DreamTeamApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DreamTeamApplication.class, args);
+    }
+
+    @Autowired
+    CardRepository cardRepository;
+
+    @Bean
+    @Profile("production")
+    public CommandLineRunner init() {
+        return args -> {
+            Card card = Card.builder().build();
+            cardRepository.save(card);
+            List<Card> cardList = cardRepository.findAll();
+            System.out.println(cardList.size());
+        };
+
     }
 
 }
