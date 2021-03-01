d0fb3dbca1e65ae7384517aa2906c5ae49077c0d
Tharegon <dombovari11david@gmaa>
2021-03-01 16:10:53 +0100

    add CombatLogRepository

---
+++ src/main/java/com/codecool/dreamteam/repository/CombatLogRepository.java
@@ -0,0 +1,7 @@
+package com.codecool.dreamteam.repository;
+
+import com.codecool.dreamteam.entity.CombatLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CombatLogRepository extends JpaRepository<CombatLog,Long> {
+}
