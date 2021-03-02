b030e60f4b0982f5fc3fe446e2c95d43d666dc4e
Tharegon <dombovari11david@gmaa>
2021-03-02 11:13:21 +0100

    add CombatLog to battle

--- src/main/java/com/codecool/dreamteam/service/CardService.java
+++ src/main/java/com/codecool/dreamteam/service/CardService.java
@@ -2,33 +2,34 @@ package com.codecool.dreamteam.service;
 
 
 import com.codecool.dreamteam.entity.Card;
+import com.codecool.dreamteam.entity.CombatLog;
 import com.codecool.dreamteam.entity.PageUser;
 import com.codecool.dreamteam.entity.Team;
 import com.codecool.dreamteam.repository.CardRepository;
+import com.codecool.dreamteam.repository.CombatLogRepository;
 import com.codecool.dreamteam.repository.PageUserRepository;
 import com.codecool.dreamteam.repository.TeamRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.HashSet;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
 @Service
 public class CardService {
 
+    private final CombatLogRepository logRepository;
     private final PageUserRepository pageUserRepository;
     private final CardRepository cardRepository;
     private final CardCreator cardCreator;
     private final TeamRepository teamRepository;
 
-    public CardService(PageUserRepository pageUserRepository, CardRepository cardRepository, CardCreator cardCreator, TeamRepository teamRepository) {
+    public CardService(PageUserRepository pageUserRepository, CardRepository cardRepository, CardCreator cardCreator, TeamRepository teamRepository, CombatLogRepository logRepository) {
         this.pageUserRepository = pageUserRepository;
         this.cardRepository = cardRepository;
         this.cardCreator = cardCreator;
         this.teamRepository = teamRepository;
+        this.logRepository = logRepository;
     }
 
     public Set<Card> getMyCard(Long id) {
@@ -126,6 +127,7 @@ public class CardService {
         PageUser red = pageUserRepository.getOne(redId);
         Set<Card> blueTeam = blue.getTeam().getMyTeam();
         Set<Card> redTeam = red.getTeam().getMyTeam();
+        CombatLog log = CombatLog.builder().date(LocalDateTime.now()).blue(blue).red(red).build();
         int blueSkill=0;
         int redSkill=0;
         for (Card bcard: blueTeam) {
@@ -136,24 +138,34 @@ public class CardService {
 
             redSkill = redSkill + rcard.getAllSkill();
         }
-        System.out.println(blueSkill+"  "+redSkill);
+        System.out.println("Blue side points: "+blueSkill+" Red Side points: "+ redSkill);
+        log.setText("Blue side points: "+blueSkill+" Red Side points: "+ redSkill);
         if (blueSkill>=redSkill){
             blue.setWin(blue.getWin()+1);
             blue.setSilverCoin(blue.getSilverCoin()+25);
+            log.setWinner("blue");
+
             red.setLose(red.getLose()+1);
             red.setSilverCoin(red.getSilverCoin()+5);
+            log.setLoser("red");
 
         }
         else {
             red.setWin(red.getWin()+1);
             red.setSilverCoin(red.getSilverCoin()+25);
+            log.setWinner("red");
+
             blue.setSilverCoin(blue.getSilverCoin()+5);
             blue.setLose(blue.getLose()+1);
+            log.setLoser("blue");
+
         }
+
         red.setMatchPlayed(red.getMatchPlayed()+1);
         blue.setMatchPlayed(blue.getMatchPlayed()+1);
         pageUserRepository.save(blue);
         pageUserRepository.save(red);
+        logRepository.save(log);
     }
 }
 
