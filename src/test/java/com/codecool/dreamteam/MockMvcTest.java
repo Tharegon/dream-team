413f5a97186bc476c0bbc97120f7fee6a7f6de2a
Tharegon <dombovari11david@gmaa>
2021-04-07 15:30:01 +0200

    add testMyCardsReturnJSON

--- src/test/java/com/codecool/dreamteam/MockMvcTest.java
+++ src/test/java/com/codecool/dreamteam/MockMvcTest.java
@@ -1,14 +1,20 @@
 package com.codecool.dreamteam;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import com.codecool.dreamteam.controller.CardController;
 import com.codecool.dreamteam.service.CardService;
+import org.hamcrest.Matchers;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class MockMvcTest {
@@ -25,5 +31,12 @@ public class MockMvcTest {
     public void setUp() throws Exception {
         mockMvc = MockMvcBuilders.standaloneSetup(cardController)
                 .build();
+    }
+
+    @Test
+    public void testMyCardsReturnJSON() throws Exception{
+        mockMvc.perform(get("/myCards/3")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
