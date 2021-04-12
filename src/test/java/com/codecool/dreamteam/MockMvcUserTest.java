681ef618edaf75d0c32f3b1e9ae93c33f5824cd7
Tharegon <dombovari11david@gmaa>
2021-04-12 16:33:34 +0200

    add ledger test

--- src/test/java/com/codecool/dreamteam/MockMvcUserTest.java
+++ src/test/java/com/codecool/dreamteam/MockMvcUserTest.java
@@ -5,11 +5,19 @@ import com.codecool.dreamteam.controller.UserController;
 import com.codecool.dreamteam.service.BattleService;
 import com.codecool.dreamteam.service.CardService;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 public class MockMvcUserTest {
 
     private MockMvc mockMvc;
@@ -24,6 +32,13 @@ public class MockMvcUserTest {
     public void setUp() throws Exception {
         mockMvc = MockMvcBuilders.standaloneSetup(userController)
                 .build();
+    }
+
+    @Test
+    public void testLedgerReturnJSON() throws Exception{
+        mockMvc.perform(get("/ledger")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
 }
